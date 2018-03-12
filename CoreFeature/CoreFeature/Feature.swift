ee142346d7aa2f07cd9c6c83d29cef54e2156f0c
Mollick, Md Razib Uddin <MMollick@Ashleyfurniture.com>
2018-03-12 12:43:57 -0400

    Table cell added

---
+++ CoreFeature/CoreFeature/Feature.swift
@@ -0,0 +1,19 @@
+//
+//  Feature.swift
+//  CoreFeature
+//
+//  Created by Mollick, Md Razib Uddin on 3/12/18.
+//  Copyright © 2018 Ashley Furniture. All rights reserved.
+//
+
+import Foundation
+
+struct Feature {
+    var title: String?
+    var description: String?
+}
+
+struct FeatureList {
+     var list:[Feature]?
+}
+
---
+++ CoreFeature/CoreFeature/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  CoreFeature
+//
+//  Created by Mollick, Md Razib Uddin on 3/12/18.
+//  Copyright © 2018 Ashley Furniture. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    
+    var list:[Feature] = [
+        Feature(title: "Social Media", description: "Login with FB, Twitter & Google"),
+        Feature(title: "Mapkit", description: "Test Map from google")
+    ]
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellBasic")
+        cell?.textLabel?.text = list[indexPath.item].title
+        cell?.detailTextLabel?.text = list[indexPath.item].description
+        
+        return cell!
+    }
+    
+    
+}
